8f3ea747c2f6f083d9d8d89c1e878654a279276a
hulaanmo <inhog.cedrick@gmail.com>
2026-08-22 17:58:39 +0800

    feat: initial api commit

---
+++ api/src/test/java/com/prompt2bean/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prompt2bean.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
